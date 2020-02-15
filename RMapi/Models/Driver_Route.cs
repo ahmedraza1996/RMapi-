26270e1be19bc217d1b1482e6e72ac361db90e96
Ahmed Raza <ahmedrazamehmood@gmail.com>
2020-02-15 12:55:25 +0500

    USER and DriverRoute CRUD updated

--- RMapi/Models/Driver_Route.cs
+++ RMapi/Models/Driver_Route.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using RMapi.Shared;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +17,215 @@ namespace RMapi.Models
         public string TO { get; set; }
         public string TIMESLAB { get; set; }
         public int AVAILABLESEATS { get; set; }
+
+    }
+    public class DriverRouteManager : BaseManager
+    {
+        public static List<Driver_Route> GetRoute(string whereclause, MySqlConnection conn = null)
+        {
+            Driver_Route objDriverRoute = null;
+            List<Driver_Route> lstRoute = new List<Driver_Route>();
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                string sql = "select * from DRIVER_ROUTE";
+                if (!string.IsNullOrEmpty(whereclause))
+                    sql += " where " + whereclause;
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = sql;
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                objDriverRoute = ReaderDataRoute(reader);
+                                lstRoute.Add(objDriverRoute);
+                            }
+                        }
+                        else
+                        {
+                        }
+                    }
+                    if (isConnArgNull == true)
+                    {
+                        connection.Dispose();
+                    }
+
+
+                }
+            }
+            //endtry
+            catch (Exception ex)
+            {
+
+            }
+            return lstRoute;
+        }
+
+
+        private static Driver_Route ReaderDataRoute(MySqlDataReader reader)
+        {
+
+            Driver_Route objRoute = new Driver_Route();
+
+            objRoute.ROUTEID = Converter.IsValidInt(reader["ROUTEID"]);
+            objRoute.DRIVERID = Converter.IsValidInt(reader["DRIVERID"]);
+            objRoute.DATE = Converter.IsValidDateTime(reader["DATE"]);
+            objRoute.FROM = Converter.IsValidString(reader["FROM"]);
+            objRoute.TO = Converter.IsValidString(reader["TO"]);
+            objRoute.TIMESLAB = Converter.IsValidString(reader["TIMESLAB"]);
+            objRoute.AVAILABLESEATS = Converter.IsValidInt(reader["AVAILABLESEATS"]);
+            return objRoute;
+
+        }
+
+        public static string SaveRoute(Driver_Route objRoute, MySqlConnection conn = null, MySqlTransaction trans = null)
+        {
+            string returnMessage = "";
+            string sROUTEID = "";
+            sROUTEID = objRoute.ROUTEID.ToString(); 
+            var templstDriver = GetRoute("ROUTEID = '" + sROUTEID + "'", conn);
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    string sql;
+                    bool isEdit = true;
+                    if (templstDriver.Count <= 0)
+                    {
+                        isEdit = false;
+                        sql = @"INSERT INTO DRIVER_ROUTE(
+                                                 
+                                                DRIVERID,
+                                                DATE,
+                                                FROM ,
+                                                TO ,
+                                                TIMESLAB, 
+                                                AVAILABLESEATS,
+                                                    )
+                                                    VALUES(
+                                                      @DRIVERID,
+                                                        @DATE,
+                                                        @FROM ,
+                                                        @TO ,
+                                                        @TIMESLAB, 
+                                                        @AVAILABLESEATS,
+                                                    )";
+                    }
+                    else
+                    {
+                        sql = @"Update DRIVER_ROUTE set
+                                                    ROUTEID=@ROUTEID,  
+                                                    DRIVERID=@DRIVERID,                                               
+                                                    DATE=@DATE,
+                                                    FROM=@FROM,
+                                                    TO=@TO,
+                                                    TIMESLAB=@TIMESLAB, 
+                                                    AVAILABLESEATS=@AVAILABLESEATS
+                                                    
+
+                                                    Where DRIVERID=@DRIVERID";
+                    }
+                    if (trans != null)
+                    {
+                        command.Transaction = trans;
+                    }
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sql;
+                    if (isEdit)
+                    {
+                        command.Parameters.AddWithValue("@ROUTEID", objRoute.ROUTEID);
+                    }
+                    command.Parameters.AddWithValue("@DRIVERID", objRoute.DRIVERID);
+                    command.Parameters.AddWithValue("@DATE", objRoute.DATE);
+                    command.Parameters.AddWithValue("@FROM", objRoute.FROM);
+                    command.Parameters.AddWithValue("@TO", objRoute.TO);
+                    command.Parameters.AddWithValue("@TIMESLAB", objRoute.TIMESLAB);
+                    command.Parameters.AddWithValue("@AVAILABLESEATS", objRoute.AVAILABLESEATS);
+                  
+
+
+                    int affectedRows = command.ExecuteNonQuery();
+                    var lastInsertID = command.LastInsertedId;
+                    if (affectedRows > 0)
+                    {
+                        //    if (!isEdit)
+                        //    {
+                        //        returnMessage = lastInsertID.ToString();
+                        //    }
+                        //    else
+                        {
+                            returnMessage = Shared.Constants.MSG_OK_DBSAVE.Text;
+                        }
+
+                    }
+                    else
+                    {
+                        returnMessage = Shared.Constants.MSG_ERR_DBSAVE.Text;
+                    }
+                }
+
+                if (isConnArgNull == true)
+                {
+                    connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return returnMessage;
+        }
+
+        public static string DeleteRoute(string ROUTEID, MySqlConnection conn = null)
+        {
+            string returnMessage = "";
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    string sql;
+                    sql = @"DELETE from DRIVER_ROUTE Where ROUTE = @ROUTEID";
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sql;
+                    command.Parameters.AddWithValue("@ROUTEID", ROUTEID);
+                    int affectedRows = command.ExecuteNonQuery();
+                    if (affectedRows > 0)
+                    {
+                        returnMessage = Shared.Constants.MSG_OK_DBSAVE.Text;
+                    }
+                    else
+                    {
+                        returnMessage = Shared.Constants.MSG_ERR_DBSAVE.Text;
+                    }
+                }
+
+                if (isConnArgNull == true)
+                {
+                    connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return returnMessage;
+        }
 
     }
 }
--- RMapi/Models/User.cs
+++ RMapi/Models/User.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using RMapi.Shared;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -16,5 +19,220 @@ namespace RMapi.Models
         public string USERNAME { get; set;  }
         public string GENDER { get; set;  }
         public string PHONENUMBER { get; set;  }
+    }
+    public class UserManager : BaseManager
+    {
+        public static List<User> GetUser(string whereclause, MySqlConnection conn = null)
+        {
+            User objUser = null;
+            List<User> lstUser = new List<User>();
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                string sql = "select * from User";
+                if (!string.IsNullOrEmpty(whereclause))
+                    sql += " where " + whereclause;
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = sql;
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                objUser = ReaderDataUser(reader);
+                                lstUser.Add(objUser);
+                            }
+                        }
+                        else
+                        {
+                        }
+                    }
+                    if (isConnArgNull == true)
+                    {
+                        connection.Dispose();
+                    }
+
+
+                }
+            }
+            //endtry
+            catch (Exception ex)
+            {
+
+            }
+            return lstUser;
+        }
+
+
+        private static User ReaderDataUser(MySqlDataReader reader)
+        {
+
+            User objUser = new User();
+            objUser.USERID = Converter.IsValidInt(reader["USERID"]);
+            objUser.FIRSTNAME = Converter.IsValidString(reader["FIRSTNAME"]);
+            objUser.LASTNAME = Converter.IsValidString(reader["LASTNAME"]);
+            objUser.GENDER = Converter.IsValidString(reader["GENDER"]);
+            objUser.NU_ID = Converter.IsValidString(reader["NU_ID"]);
+            objUser.PASSWORD = Converter.IsValidString(reader["PASSWORD"]);
+            objUser.PHONENUMBER = Converter.IsValidString(reader["PHONENUMBER"]);
+            objUser.USERNAME = Converter.IsValidString(reader["USERNAME"]);
+
+
+            //will add for image
+            return objUser;
+
+        }
+
+        public static string SaveUser(User objUser, MySqlConnection conn = null, MySqlTransaction trans = null)
+        {
+            string returnMessage = "";
+            string sUSERID = "";
+            sUSERID = objUser.USERID.ToString();
+            var templstDriver = GetUser("USERID = '" + sUSERID + "'", conn);
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    string sql;
+                    bool isEdit = true;
+                    if (templstDriver.Count <= 0)
+                    {
+                        isEdit = false;
+                        sql = @"INSERT INTO USER(
+                                                    
+                                                  FIRSTNAME,
+                                                  LASTNAME,
+                                                  GENDER,
+                                                  NU_ID,
+                                                  PASSWORD,
+                                                  PHONENUMBER,
+                                                  USERNAME
+
+                                                 
+                                                    )
+                                                    VALUES(
+                                                     @FIRSTNAME,
+                                                  @LASTNAME,
+                                                  @GENDER,
+                                                  @NU_ID,
+                                                  @PASSWORD,
+                                                  @PHONENUMBER,
+                                                  @USERNAME
+                                                    )";
+                    }
+                    else
+                    {
+                        sql = @"Update USER set
+                                                  USERID=@USERID,
+                                                  FIRSTNAME=@FIRSTNAME,
+                                                  LASTNAME=@LASTNAME,
+                                                  GENDER=@GENDER,
+                                                  NU_ID=@NU_ID,
+                                                  PASSWORD=@PASSWORD,
+                                                  PHONENUMBER=@PHONENUMBER,
+                                                  USERNAME=@USERNAME
+                                                    Where USERID=@USERID";
+                    }
+                    if (trans != null)
+                    {
+                        command.Transaction = trans;
+                    }
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sql;
+                    if (isEdit)
+                    {
+                        command.Parameters.AddWithValue("@USERID",objUser.USERID);
+                    }
+                    command.Parameters.AddWithValue("@FIRSTNAME", objUser.FIRSTNAME);
+                    command.Parameters.AddWithValue("@LASTNAME", objUser.LASTNAME);
+                    command.Parameters.AddWithValue("@USERNAME", objUser.USERNAME);
+                    command.Parameters.AddWithValue("@PASSWORD", objUser.PASSWORD);
+                    command.Parameters.AddWithValue("@GENDER", objUser.GENDER);
+                    command.Parameters.AddWithValue("@NU_ID", objUser.NU_ID);
+                    command.Parameters.AddWithValue("@PHONENUMBER", objUser.PHONENUMBER);
+
+
+                    int affectedRows = command.ExecuteNonQuery();
+                    var lastInsertID = command.LastInsertedId;
+                    if (affectedRows > 0)
+                    {
+                        //    if (!isEdit)
+                        //    {
+                        //        returnMessage = lastInsertID.ToString();
+                        //    }
+                        //    else
+                        {
+                            returnMessage = Shared.Constants.MSG_OK_DBSAVE.Text;
+                        }
+
+                    }
+                    else
+                    {
+                        returnMessage = Shared.Constants.MSG_ERR_DBSAVE.Text;
+                    }
+                }
+
+                if (isConnArgNull == true)
+                {
+                    connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return returnMessage;
+        }
+
+        public static string DeleteUser(string USERID, MySqlConnection conn = null)
+        {
+            string returnMessage = "";
+            try
+            {
+                bool isConnArgNull = (conn != null) ? false : true;
+                MySqlConnection connection = (conn != null) ? conn : PrimaryConnection();
+                tryOpenConnection(connection);
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    string sql;
+                    sql = @"DELETE from USER Where USERID = @USERID";
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sql;
+                    command.Parameters.AddWithValue("@USERID", USERID);
+                    int affectedRows = command.ExecuteNonQuery();
+                    if (affectedRows > 0)
+                    {
+                        returnMessage = Shared.Constants.MSG_OK_DBSAVE.Text;
+                    }
+                    else
+                    {
+                        returnMessage = Shared.Constants.MSG_ERR_DBSAVE.Text;
+                    }
+                }
+
+                if (isConnArgNull == true)
+                {
+                    connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return returnMessage;
+        }
+
     }
 }
